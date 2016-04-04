d6166d5cd26070a2d5d071ff88bfc70f31e46bdf
Alberto <a.escalante.del.valle@cern.ch>
2016-04-04 11:07:43 +0200

    update

--- ExampleScaleCorrection.C
+++ ExampleScaleCorrection.C
@@ -1,7 +1,7 @@
 #include "include/GeneralizedEndpoint.h"
 
 int main () {
-  //Example muon.
+  //Random example muon.
 
   float MuonPt= 500.; //Input pt in GeV
   int MuonCharge = 1; // Input charge +1 or -1.
@@ -9,23 +9,22 @@ int main () {
   float MuonPhi=150.; //Muon phi in degrees (-180, 180), Reminder if Radians use: MuonPhi = (MuonPhiInRad*180)/Pi.
   int mode=0; //0 Nominal, 1 Systematic Up, 2 systematic Down.
 
-
   /// To be applied on simulation only.
   GeneralizedEndpoint ScaleSystematic;
 
   //Get Nominal Correction.
   ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, mode);
-  // //Get SystematicUp Correction.
+  //Get SystematicUp Correction.
   ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, 1);
-  // //Get SystematicDown Correction.
-  ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, 2);
-  
+  //Get SystematicDown Correction.
+  ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, 2);  
+  //Get Nominal Correction with verbose =1.
+  ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, 0, 1);
 
   // //For test.
-  // // ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, 220., mode);
-  // // ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, 3.5, MuonPhi, mode);
-  // // ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, -2);
-  // // ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, -1);
-  ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, 0, 1);
+  // ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, 220., mode);
+  // ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, 3.5, MuonPhi, mode);
+  // ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, -2);
+  // ScaleSystematic.GeneralizedEndpointPt(MuonPt, MuonCharge, MuonEta, MuonPhi, -1);
 
 }
