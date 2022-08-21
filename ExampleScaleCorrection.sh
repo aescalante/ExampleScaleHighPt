2120a87981e1f359d11bf6a1a3c28fcd5f596cd5
Alberto Escalante <a.escalante.del.valle@cern.ch>
2022-08-21 13:04:15 +0200

    Updated compiler instructions, tested in ROOT 6.26.04_2

--- ExampleScaleCorrection.sh
+++ ExampleScaleCorrection.sh
@@ -3,9 +3,9 @@
 # Run the following to compile and execute the example.
 #
 rm ExampleScaleCorrection.exe >& /dev/null
-ROOTLIBS="-L/usr/lib64 `$ROOTSYS/bin/root-config --glibs` -lMathCore -lMinuit"
+ROOTLIBS="-L/usr/lib `$ROOTSYS/bin/root-config --glibs` -lMathCore -lMinuit"
 ROOTINCDIR=`$ROOTSYS/bin/root-config --incdir`
-g++ -std=c++11 -I${ROOTINCDIR} -o ExampleScaleCorrection.exe \
+g++ -std=c++14 -I${ROOTINCDIR} -o ExampleScaleCorrection.exe \
     include/GeneralizedEndpoint.cc ${ROOTLIBS} ExampleScaleCorrection.C
 #
 ./ExampleScaleCorrection.exe
